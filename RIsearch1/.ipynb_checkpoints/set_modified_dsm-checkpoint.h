e2711be7e6bed9e3c12a583ee8dc1de5f2265430
Din-Saadon <din.saadon1@gmail.com>
2025-04-14 18:02:35 +0300

    _

--- RIsearch1/.ipynb_checkpoints/set_modified_dsm-checkpoint.h
+++
@@ -1,9 +0,0 @@
-#ifndef SET_MODIFIED_H  // Replace HEADER_NAME_H with a unique name
-#define SET_MODIFIED_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h> 
-#include <string.h>
-void set_modified();
-
-#endif // HEADER_NAME_H
